1abcd53839b77667520a90a50cb20d02dce030f9
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2024-11-16 18:31:38 +0530

    Update __init__.py

--- Tanjiro/__init__.py
+++ Tanjiro/__init__.py
@@ -5,7 +5,7 @@ API_ID = int(os.environ.get('API_ID', ''))
 API_HASH = os.environ.get('API_HASH', '')  
 SESSION = os.environ.get('SESSION', '')  
 
-SUDO = [1867106198]  
+SUDO = [6200605339]  
 
 TanjiroUb = Client(
         "ub",
